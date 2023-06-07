b690be6a0ea82dda1c81239a3adbbceba16a6918
Sümeyye Şahin <smyysahin823@gmail.com>
2023-06-08 02:53:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
-import {View,Text, SafeAreaView, StyleSheet, TouchableOpacity} from "react-native";
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 import Card from './components/card/Card';
 
-function App(){
-  return(
+function App() {
+  return (
     <SafeAreaView>
-      <Card title="Özdemir Asaf" text="Kaybedeceğini bildiğin halde neden devam ediyorsun dedi. Öleceğini bildiği halde yaşadığını unutmuştu o ama bozmadım." />
-      <Card title="Atatürk" text="Yurtta sulh cihanda sulh!"/>
-
+      <Card
+        title="Özdemir Asaf"
+        text="Kaybedeceğini bildiğin halde neden devam ediyorsun dedi. Öleceğini bildiği halde yaşadığını unutmuştu o ama bozmadım."
+      />
+      <Card title="Atatürk" text="Yurtta sulh cihanda sulh!" />
     </SafeAreaView>
   );
 }
